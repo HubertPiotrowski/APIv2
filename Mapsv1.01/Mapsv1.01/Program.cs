3f655b8bd004ac0d173be99c07cbd2308ca78687
Hubert Piotrowski <hubert@shiftforex.com>
2018-07-24 09:51:45 +0200

    no message

---
+++ Mapsv1.01/Mapsv1.01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Mapsv1._01
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Broker Capxm = new Broker()
+            {
+                server = "api.pro.we.01.currentdesk.com:444",
+                id = 29,
+                user = "api@capxm.com",
+                password = "123trade"
+            };
+
+            Dictionary<int, Broker> dictionaryBrokers = new Dictionary<int, Broker>();
+            dictionaryBrokers.Add(Capxm.id, Capxm);
+
+            Broker broker101 = dictionaryBrokers[29];
+
+            Console.WriteLine("ID = {0}, email = {1}", broker101.id, broker101.user);
+        }
+    }
+
+    public class Broker
+    {
+        public string server { get; set; }
+        public int id { get; set; }
+        public string user { get; set; }
+        public string password { get; set; }
+    };
+
+}
---
+++ Mapsv1.01/Mapsv1.01/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Mapsv1.01")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mapsv1.01")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("aee230b5-c895-40b8-9198-70e12dbb97c6")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
